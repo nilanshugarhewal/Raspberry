146b6acb74fa69191aed4cca94218bd06026f6e5
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2026-07-01 17:05:17 +0530

    Created some important methods in Parser.ts.

--- compiler/src/parser/Parser.ts
+++ compiler/src/parser/Parser.ts
@@ -1,6 +1,11 @@
 // This is the parser file for the Raspberry programming language.
 
 import { Token } from "../lexer/tokens/Token";
+import { TokenType } from "../lexer/tokens/TokenType";
+
+// Literals
+import { NumberLiteral } from "./ast/expressions/NumberLiteral";
+import { StringLiteral } from "./ast/expressions/StringLiteral";
 
 export class Parser {
   private readonly tokens: Token[];
@@ -10,4 +15,46 @@ export class Parser {
     this.tokens = tokens;
     this.currentIndex = 0;
   }
+
+  // ------------------------------------------->
+  // NAVIGATIONS ------------------------------->
+  // ------------------------------------------->
+
+  private get currentToken(): Token | undefined {
+    return this.tokens[this.currentIndex];
+  }
+
+  private advance(): void {
+    this.currentIndex++;
+  }
+
+  private get isAtEnd(): boolean {
+    return this.currentToken?.type === TokenType.EOF;
+  }
+
+  // ------------------------------------------->
+  // LITERALS ---------------------------------->
+  // ------------------------------------------->
+
+  private consume(expected: TokenType): Token {
+    const token = this.currentToken;
+
+    if (token === undefined) {
+      throw new Error("Unexpected end of file.");
+    }
+
+    if (token.type !== expected) {
+      throw new Error(
+        `Expected ${TokenType[expected]}, but found ${TokenType[token.type]}.`,
+      );
+    }
+
+    this.advance();
+
+    return token;
+  }
+
+  // public parseNumberLiteral(): NumberLiteral {
+
+  // }
 }
